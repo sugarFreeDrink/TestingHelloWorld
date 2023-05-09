7fa4d6f466550115306b6e2caa1ee809e0e23f29
hello <qq942391131@gmail.com>
2023-05-09 15:54:14 +0200

    init src

---
+++ src/main/java/com/itheima/controller/TestingHelloWorldApplication.java
@@ -0,0 +1,20 @@
+package com.itheima.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/hello")
+public class TestingHelloWorldApplication {
+    @GetMapping("")
+    public String hello(){
+        System.out.println("hello world...");
+        return "OK";
+    }
+
+
+
+}
